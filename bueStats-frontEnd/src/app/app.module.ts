d4dadf5e7daceb8a0485825785ac98c8b7256852
marc10010 <75836335+marc10010@users.noreply.github.com>
2022-10-19 23:40:36 +0200

    commit to update

--- bueStats-frontEnd/src/app/app.module.ts
+++ bueStats-frontEnd/src/app/app.module.ts
@@ -1,37 +1,18 @@
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from "./material.module";
-import { FormsModule } from "@angular/forms";
-import { MatDialogModule } from "@angular/material/dialog";
-import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './components/header/header.component';
-import { LOCALE_ID } from '@angular/core';
-import es from '@angular/common/locales/es';
-import { registerLocaleData } from '@angular/common';
-registerLocaleData(es);
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule,
     BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    MaterialModule,
-    MatDialogModule,
   ],
   exports: [
   ],
-  providers: [ { provide: LOCALE_ID, useValue: 'es-ES' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
